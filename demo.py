30e5406b826cfb0e8bf16bc7ee85e3958b5789eb
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2025-03-19 10:17:30 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,32 @@
+import keyword
+print(keyword.kwlist)
+
+
+print("Hello world")
+
+li = [5,6,23,2]
+print(li[0])
+li[0] = 100
+print(li[0])
+
+
+# li = (5,6,23,2)
+# print(li[0])
+# del li[0] 
+# print(li[0])
+
+print(2**5)
+
+print("""Twinkle, twinkle, little star,
+How I wonder what you are!
+Up above the world so high,
+Like a diamond in the sky.
+
+When the blazing sun is gone,
+When he nothing shines upon,
+Then you show your little light,
+Twinkle, twinkle, all the night.""")
+
+n = 56
+
+print("Your value is",n)
